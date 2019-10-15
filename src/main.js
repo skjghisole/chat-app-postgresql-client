0f93dc2a7e9752640490b91f1d346c36e7ce3fd0
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-15 15:41:17 -0400

    managed tow get websockets working

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import socketIO from 'socket.io-client'
+
+const socket = socketIO.connect('localhost:2020')
+socket.on('sayHiFromServer', () => {
+	socket.emit('userConnected', { name: "karl" })
+})
+
+socket.on('newUser', ({ msg }) => {
+	console.log(msg)
+})
+
 Vue.config.productionTip = false
 
 new Vue({
