4964cb537f38b0dd77c0ec29af9ac0809e98ab59
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-15 17:23:58 -0400

    created a sticky header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <Header title="ChitChat" />
+    <div id="app-container">
+
+    </div>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
+<script>
+  import Header from './components/layout/Header'
+  export default {
+    name: "App",
+    components: {
+      Header
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app-container {
+    max-width: 60%;
+    margin: auto;
+  }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="container">
+		<h1 id="title">
+			{{title}}
+		</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+		props: ['title'],
+		data() {
+			title: ''
+		}
+	}
+</script>
+
+<style lang="css" scoped>
+	.container {
+		height: 70px;
+		background-color: coral;
+		font-family: Helvetica, Arial;
+		position: sticky;
+		top: 0;
+	}
+
+	h1 {
+
+	}
+
+	#title {
+		padding: 20px 10px;
+		letter-spacing: 0.2em;
+	}
+
+</style>
