1959df1b5462d3400f59b7697a668a5b5378af1e
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-17 21:40:56 -0400

    added user authentication

--- src/store/modules/User.js
+++ src/store/modules/User.js
@@ -1,17 +1,31 @@
+import jwt from 'jsonwebtoken'
+
 const state = {
 	userLogin: {
 		email: '',
 		password: ''
-	}
+	},
+	user: null,
+	token: null
 }
 
 const getters = {
 	getUserLogin: (state) => state.userLogin,
+	getLoggedInUser: (state) => state.user
 }
 
 const actions = {
 	attemptHandleInputChange: ({ commit }, { name, value }) => {
 		commit('setUserLogin', { name, value })
+	},
+	attemptAuthenticateUser: async ({ commit, state }) => {
+		try {
+			const token = localStorage.getItem('token')
+			if (!token && !state.token) throw new Error ('User not authenticated!')
+			commit('saveToken', token)
+		} catch (e) {
+			console.log(e)
+		}
 	},
 	attemptLogin: async ({ commit, state }) => {
 		const { userLogin } = state
@@ -23,22 +37,26 @@ const actions = {
 					'Content-Type': 'application/json'
 				}
 			})
-			console.log(res)
 			const data = await res.json()
-			console.log(data)
+			const { message, token } = data
+			localStorage.setItem('token', token)
 		} catch (e) {
 			console.log(e)
 		}
-		
-			// commit('attemptLogin', {
-			// 	email,
-			// 	password
-			// })
 	}
 }
 
 const mutations = {
-	setUserLogin: (state, { name, value }) => (state.userLogin[name] = value)
+	setUserLogin: (state, { name, value }) => (state.userLogin[name] = value),
+	saveToken: async (state, token) => {
+		try {
+			const user = await jwt.verify(token, process.env.VUE_APP_SECRET_KEY)
+			state.token = token
+			state.user = user
+		} catch(e) {
+			console.error(e)
+		}
+	}
 }
 
 export default {
